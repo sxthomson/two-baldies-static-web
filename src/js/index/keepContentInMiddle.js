8ac49dc1c237e04632d21c9a1a88432b3e7ae858
Scott Thomson <scott.thomson@just-eat.com>
2018-09-20 17:17:04 +0100

    Slight code style change

--- src/js/index/keepContentInMiddle.js
+++ src/js/index/keepContentInMiddle.js
@@ -12,14 +12,14 @@ const changeMarginTop = ($element, $elementsToCenter, windowWidth) => {
     const innerHeight = $element.outerHeight();   
     if (windowWidth < 768) {
         $($elementsToCenter[0]).css('margin-top', 'inherit');
-    } else {
-        let currentHeight = 0;
-
-        $elementsToCenter.each(index => {
-            currentHeight += $elementsToCenter[index].clientHeight;
-        });
-    
-        const marginTopPx = (innerHeight - currentHeight) / 2;
-        $($elementsToCenter[0]).css('margin-top', marginTopPx);
+        return;
     }
+    let currentHeight = 0;
+
+    $elementsToCenter.each(index => {
+        currentHeight += $elementsToCenter[index].clientHeight;
+    });
+
+    const marginTopPx = (innerHeight - currentHeight) / 2;
+    $($elementsToCenter[0]).css('margin-top', marginTopPx);    
 };
--- src/js/index/setHeight.js
+++ src/js/index/setHeight.js
@@ -1,14 +1,18 @@
 export const setHeight = ($wrapper, $dynamicElement) => {
     const $window = $(window);
-    
-    setHeightOfElement($wrapper, $dynamicElement);
+
+    setHeightOfElement($wrapper, $dynamicElement, $window.outerWidth());
 
     $window.resize(() => {
-        setHeightOfElement($wrapper, $dynamicElement);
+        setHeightOfElement($wrapper, $dynamicElement, $window.outerWidth());
     });
 };
 
-const setHeightOfElement = ($wrapper, $dynamicElement) => {
-    var dynamicElementHeight = $($dynamicElement).height();
+const setHeightOfElement = ($wrapper, $dynamicElement ,windowWidth) => {
+    if (windowWidth < 768) {
+        $wrapper.height("auto");
+        return;
+    }
+    const dynamicElementHeight = $($dynamicElement).height();
     $wrapper.height(dynamicElementHeight);
 };
