15199ca125a5a370fe302013f0d4ccf92f013e36
Scott Thomson <scott.89@gmail.com>
2019-06-05 21:12:15 +0100

    Try adding a deploy shell script and invoke via bash entrypoint

---
+++ deploy/deploy-staging.sh
@@ -0,0 +1,38 @@
+gsutil -m rsync -r -d "../dist" gs://$STORAGE_ACCOUNT_NAME
+
+SetMetadata "Content-Type" "text/html" "$STORAGE_ACCOUNT_NAME" "**.html"
+
+gsutil setmeta -m -h "Content-Type:text/html" gs://$STORAGE_ACCOUNT_NAME/*.html
+gsutil setmeta -m -h "Cache-Control:private, max-age=0, no-transform" gs://$STORAGE_ACCOUNT_NAME/*.html
+
+gsutil web set -m index.html gs://$STORAGE_ACCOUNT_NAME
+
+gsutil setmeta -m -h "Content-Type:text/html" gs://$STORAGE_ACCOUNT_NAME/**.html
+gsutil setmeta -m -h "Cache-Control:no-cache" gs://$STORAGE_ACCOUNT_NAME/**.html
+
+gsutil setmeta -m -h "Content-Type:image/png" gs://$STORAGE_ACCOUNT_NAME/**.png
+gsutil setmeta -m -h "Cache-Control:public, max-age=31536000" gs://$STORAGE_ACCOUNT_NAME/**.png
+
+gsutil setmeta -m -h "Content-Type:image/svg+xml" gs://$STORAGE_ACCOUNT_NAME/**.svg
+gsutil setmeta -m -h "Cache-Control:public, max-age=31536000" gs://$STORAGE_ACCOUNT_NAME/**.svg
+
+gsutil setmeta -m -h "Content-Type:text/javascript" gs://$STORAGE_ACCOUNT_NAME/**.js
+gsutil setmeta -m -h "Cache-Control:public, max-age=31536000" gs://$STORAGE_ACCOUNT_NAME/**.js
+
+gsutil setmeta -m -h "Content-Type:text/css" gs://$STORAGE_ACCOUNT_NAME/**.css
+gsutil setmeta -m -h "Cache-Control:public, max-age=31536000" gs://$STORAGE_ACCOUNT_NAME/**.css
+
+gsutil setmeta -m -h "Content-Type:application/x-font-ttf" gs://$STORAGE_ACCOUNT_NAME/**.eot
+gsutil setmeta -m -h "Cache-Control:public, max-age=31536000" gs://$STORAGE_ACCOUNT_NAME/**.ttf
+
+gsutil setmeta -m -h "Content-Type:application/application/vnd.ms-fontobject" gs://$STORAGE_ACCOUNT_NAME/**.eot
+gsutil setmeta -m -h "Cache-Control:public, max-age=31536000" gs://$STORAGE_ACCOUNT_NAME/**.eot
+
+gsutil setmeta -m -h "Content-Type:application/application/font-woff" gs://$STORAGE_ACCOUNT_NAME/**.woff
+gsutil setmeta -m -h "Cache-Control:public, max-age=31536000" gs://$STORAGE_ACCOUNT_NAME/**.woff
+
+gsutil setmeta -m -h "Content-Type:application/application/font-woff" gs://$STORAGE_ACCOUNT_NAME/**.woff
+gsutil setmeta -m -h "Cache-Control:public, max-age=31536000" gs://$STORAGE_ACCOUNT_NAME/**.woff
+
+gsutil setmeta -m -h "Content-Type:application/application/font-woff2" gs://$STORAGE_ACCOUNT_NAME/**.woff2
+gsutil setmeta -m -h "Cache-Control:public, max-age=31536000" gs://$STORAGE_ACCOUNT_NAME/**.woff2
