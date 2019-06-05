195e31b77168db058fc8447cfdd7a3b2975267ef
Scott Thomson <scott.89@gmail.com>
2019-06-05 21:23:41 +0100

    Remove trailing slash

--- deploy/deploy-staging.sh
+++ deploy/deploy-staging.sh
@@ -1,4 +1,4 @@
-gsutil -m rsync -r -d "/dist/" gs://$STORAGE_ACCOUNT_NAME
+gsutil -m rsync -r -d "/dist" gs://$STORAGE_ACCOUNT_NAME
 
 SetMetadata "Content-Type" "text/html" "$STORAGE_ACCOUNT_NAME" "**.html"
 
